329c0e80883a8bc5c5680bb2d8d1146a72f8afb4
Deep Rana <deeprana0316@gmail.com>
2026-03-31 18:30:51 +0530

    Added Money Manager project

--- js/app.js
+++ js/app.js
@@ -1,19 +1,15 @@
 let transactions = [];
-
 document.getElementById("saveBtn").addEventListener("click", addTransaction);
-
 function addTransaction() {
   let amount = document.getElementById("amount").value;
   let date = document.getElementById("date").value;
   let category = document.getElementById("category").value;
   let sub = document.getElementById("subCategory").value;
   let desc = document.getElementById("description").value;
-
   if (amount === "" || category === "") {
     alert("Please fill required fields");
     return;
   }
-
   let transaction = {
     id: Date.now(),
     amount: Number(amount),
@@ -22,28 +18,20 @@ function addTransaction() {
     subCategory: sub,
     description: desc
   };
-
-  // ✅ ADD HERE (after creating object)
   transactions.push(transaction);
-
-  saveToLocal();        // ✅ SAVE DATA
-
-  displayTransactions(); // ✅ UPDATE TABLE
-  updateSummary();       // ✅ UPDATE TOTAL
-
-  // (optional) clear form
+  saveToLocal();
+  displayTransactions();
+  updateSummary();
   document.getElementById("amount").value = "";
   document.getElementById("date").value = "";
   document.getElementById("category").value = "";
   document.getElementById("subCategory").value = "";
   document.getElementById("description").value = "";
 }
-
 function displayTransactions() {
   let list = document.getElementById("list");
   list.innerHTML = "";
-
-  transactions.forEach(tx => {
+  transactions.forEach(function(tx) {
     let row = `
       <tr>
         <td>${tx.date}</td>
@@ -56,50 +44,42 @@ function displayTransactions() {
         </td>
       </tr>
     `;
-
     list.innerHTML += row;
   });
 }
-
 function deleteTransaction(id) {
   let confirmDelete = confirm("Are you sure?");
   if (!confirmDelete) return;
-
-  transactions = transactions.filter(tx => tx.id !== id);
-  saveToLocal(); 
+  transactions = transactions.filter(function(tx) {
+    return tx.id !== id;
+  });
+  saveToLocal();
   displayTransactions();
   updateSummary();
 }
-
 function updateSummary() {
   let income = 0;
   let expense = 0;
-
-  transactions.forEach(tx => {
+  transactions.forEach(function(tx) {
     if (tx.category === "Income") {
       income += tx.amount;
     } else {
       expense += tx.amount;
     }
   });
-
   document.getElementById("income").textContent = income;
   document.getElementById("expense").textContent = expense;
   document.getElementById("balance").textContent = income - expense;
 }
-
 function saveToLocal() {
   localStorage.setItem("data", JSON.stringify(transactions));
 }
-
 function loadFromLocal() {
   let data = localStorage.getItem("data");
-
   if (data) {
     transactions = JSON.parse(data);
     displayTransactions();
     updateSummary();
   }
 }
-
 window.onload = loadFromLocal;
